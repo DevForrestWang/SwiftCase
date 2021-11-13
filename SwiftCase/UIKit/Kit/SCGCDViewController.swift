033ae88e034db83286c7602585b186dec7ac6d5b
wangfd <wangfd@gyist.com>
2021-11-13 17:39:11 +0800

    add gcd ui kit

--- SwiftCase/UIKit/Kit/SCGCDViewController.swift
+++ SwiftCase/UIKit/Kit/SCGCDViewController.swift
@@ -11,6 +11,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+import SnapKit
+import Then
 import UIKit
 
 class SCGCDViewController: BaseViewController {
@@ -39,6 +41,32 @@ class SCGCDViewController: BaseViewController {
 
     // MARK: - IBActions
 
+    @objc private func serialSyncAction() {
+        yxc_debugPrint("serialSyncAction")
+    }
+
+    @objc private func serialAsyncAction() {
+        yxc_debugPrint("serialAsyncAction")
+    }
+
+    @objc private func concurrentSyncAction() {
+        yxc_debugPrint("concurrentSyncAction")
+    }
+
+    @objc private func concurrentAsyncAction() {
+        yxc_debugPrint("concurrentAsyncAction")
+    }
+
+    @objc private func downImageInGroupAction() {
+        yxc_debugPrint("downImageInGroupAction")
+        indicator1.startAnimating()
+        indicator2.startAnimating()
+    }
+
+    @objc private func dispatchSemaphoreAction() {
+        yxc_debugPrint("dispatchSemaphoreAction")
+    }
+
     // MARK: - Private
 
     // MARK: - UI
@@ -46,11 +74,160 @@ class SCGCDViewController: BaseViewController {
     func setupUI() {
         title = "Grand Central Dispath"
         view.backgroundColor = .white
+
+        view.addSubview(serialSyncBtn)
+        view.addSubview(serialAsyncBtn)
+        view.addSubview(concurrentSyncBtn)
+        view.addSubview(concurrentAsyncBtn)
+        view.addSubview(downImageInGroupBtn)
+        view.addSubview(dispatchSemaphoreBtn)
+
+        view.addSubview(imageView1)
+        view.addSubview(imageView2)
+
+        imageView1.addSubview(indicator1)
+        imageView2.addSubview(indicator2)
     }
 
     // MARK: - Constraints
 
-    func setupConstraints() {}
+    func setupConstraints() {
+        serialSyncBtn.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.width.equalTo(GlobalConfig.gScreenWidth / 2 - 25)
+            make.top.equalTo(20)
+            make.left.equalTo(view).offset(15)
+        }
+
+        serialAsyncBtn.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.width.equalTo(GlobalConfig.gScreenWidth / 2 - 25)
+            make.top.equalTo(20)
+            make.right.equalTo(view).offset(-15)
+        }
+
+        concurrentSyncBtn.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.width.equalTo(GlobalConfig.gScreenWidth / 2 - 25)
+            make.top.equalTo(serialSyncBtn.snp.bottom).offset(20)
+            make.left.equalTo(view).offset(15)
+        }
+
+        concurrentAsyncBtn.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.width.equalTo(GlobalConfig.gScreenWidth / 2 - 25)
+            make.top.equalTo(serialAsyncBtn.snp.bottom).offset(20)
+            make.right.equalTo(view).offset(-15)
+        }
+
+        downImageInGroupBtn.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.width.equalTo(GlobalConfig.gScreenWidth / 2 - 25)
+            make.top.equalTo(concurrentSyncBtn.snp.bottom).offset(20)
+            make.left.equalTo(view).offset(15)
+        }
+
+        dispatchSemaphoreBtn.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.width.equalTo(GlobalConfig.gScreenWidth / 2 - 25)
+            make.top.equalTo(concurrentAsyncBtn.snp.bottom).offset(20)
+            make.right.equalTo(view).offset(-15)
+        }
+
+        indicator1.snp.makeConstraints { make in
+            make.width.height.equalTo(20)
+            make.center.equalToSuperview()
+        }
+
+        indicator2.snp.makeConstraints { make in
+            make.width.height.equalTo(20)
+            make.center.equalToSuperview()
+        }
+
+        imageView1.snp.makeConstraints { make in
+            make.height.equalTo(223)
+            make.width.equalToSuperview()
+            make.top.equalTo(downImageInGroupBtn.snp.bottom).offset(20)
+        }
+
+        imageView2.snp.makeConstraints { make in
+            make.height.equalTo(223)
+            make.width.equalToSuperview()
+            make.top.equalTo(imageView1.snp.bottom).offset(20)
+        }
+    }
 
     // MARK: - Property
+
+    let serialSyncBtn = UIButton().then {
+        $0.backgroundColor = .orange
+        $0.setTitle("Serial&Sync", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 3
+        $0.addTarget(self, action: #selector(serialSyncAction), for: .touchUpInside)
+    }
+
+    let serialAsyncBtn = UIButton().then {
+        $0.backgroundColor = .orange
+        $0.setTitle("Serial&Async", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 3
+        $0.addTarget(self, action: #selector(serialAsyncAction), for: .touchUpInside)
+    }
+
+    let concurrentSyncBtn = UIButton().then {
+        $0.backgroundColor = .orange
+        $0.setTitle("Concurrent&Sync", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 3
+        $0.addTarget(self, action: #selector(concurrentSyncAction), for: .touchUpInside)
+    }
+
+    let concurrentAsyncBtn = UIButton().then {
+        $0.backgroundColor = .orange
+        $0.setTitle("Concurrent&Async", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 3
+        $0.addTarget(self, action: #selector(concurrentAsyncAction), for: .touchUpInside)
+    }
+
+    let downImageInGroupBtn = UIButton().then {
+        $0.backgroundColor = .orange
+        $0.setTitle("Load Image in Group", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 3
+        $0.addTarget(self, action: #selector(downImageInGroupAction), for: .touchUpInside)
+    }
+
+    let dispatchSemaphoreBtn = UIButton().then {
+        $0.backgroundColor = .orange
+        $0.setTitle("DispatchSemaphore", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 3
+        $0.addTarget(self, action: #selector(dispatchSemaphoreAction), for: .touchUpInside)
+    }
+
+    let imageView1 = UIImageView().then {
+        $0.contentMode = .scaleToFill
+        $0.image = UIImage(named: "placeholder")
+    }
+
+    let imageView2 = UIImageView().then {
+        $0.contentMode = .scaleToFill
+        $0.image = UIImage(named: "placeholder")
+    }
+
+    let indicator1 = UIActivityIndicatorView().then {
+        $0.style = UIActivityIndicatorView.Style.medium
+    }
+
+    let indicator2 = UIActivityIndicatorView().then {
+        $0.style = UIActivityIndicatorView.Style.medium
+    }
 }
--- SwiftCase/UIKit/Kit/SCOperationViewController.swift
+++ SwiftCase/UIKit/Kit/SCOperationViewController.swift
@@ -11,6 +11,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+import SnapKit
+import Then
 import UIKit
 
 class SCOperationViewController: BaseViewController {
@@ -39,6 +41,14 @@ class SCOperationViewController: BaseViewController {
 
     // MARK: - IBActions
 
+    @objc private func invocationOperationAction() {
+        yxc_debugPrint("invocationOperationAction")
+    }
+
+    @objc private func blockOperationAction() {
+        yxc_debugPrint("blockOperationAction")
+    }
+
     // MARK: - Private
 
     // MARK: - UI
@@ -46,11 +56,69 @@ class SCOperationViewController: BaseViewController {
     func setupUI() {
         title = "Cocoa Operation"
         view.backgroundColor = .white
+
+        view.addSubview(invocationOperationBtn)
+        view.addSubview(blockOperationBtn)
+
+        view.addSubview(imageView1)
+        imageView1.addSubview(indicator1)
     }
 
     // MARK: - Constraints
 
-    func setupConstraints() {}
+    func setupConstraints() {
+        invocationOperationBtn.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.width.equalTo(GlobalConfig.gScreenWidth / 2 - 25)
+            make.top.equalTo(20)
+            make.left.equalTo(view).offset(15)
+        }
+
+        blockOperationBtn.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.width.equalTo(GlobalConfig.gScreenWidth / 2 - 25)
+            make.top.equalTo(20)
+            make.right.equalTo(view).offset(-15)
+        }
+
+        indicator1.snp.makeConstraints { make in
+            make.width.height.equalTo(20)
+            make.center.equalToSuperview()
+        }
+
+        imageView1.snp.makeConstraints { make in
+            make.height.equalTo(223)
+            make.width.equalToSuperview()
+            make.top.equalTo(invocationOperationBtn.snp.bottom).offset(20)
+        }
+    }
 
     // MARK: - Property
+
+    let invocationOperationBtn = UIButton().then {
+        $0.backgroundColor = .orange
+        $0.setTitle("InvocationOperation", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 3
+        $0.addTarget(self, action: #selector(invocationOperationAction), for: .touchUpInside)
+    }
+
+    let blockOperationBtn = UIButton().then {
+        $0.backgroundColor = .orange
+        $0.setTitle("BlockOperation", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 3
+        $0.addTarget(self, action: #selector(blockOperationAction), for: .touchUpInside)
+    }
+
+    let imageView1 = UIImageView().then {
+        $0.contentMode = .scaleToFill
+        $0.image = UIImage(named: "placeholder")
+    }
+
+    let indicator1 = UIActivityIndicatorView().then {
+        $0.style = UIActivityIndicatorView.Style.medium
+    }
 }
