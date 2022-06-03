cd695f247ebfd93c00d264fb5f6510453f30db1c
forrestwang <forrestwang@aliyun.com>
2022-06-03 23:13:19 +0800

    "MF"

--- SwiftCase/Utility/SCDeviceInfo.swift
+++ SwiftCase/Utility/SCDeviceInfo.swift
@@ -47,10 +47,10 @@ let isIphone12_13 = (device_iPhone12_13m || device_iPhone12_13 || device_iPhone1
 
 var isFullScreen: Bool {
     if #available(iOS 11, *) {
-        guard let w = UIApplication.shared.delegate?.window, let unwrapedWindow = w else {
+        guard let unwrapedWindow = UIWindow.key else {
             return false
         }
-
+        
         if unwrapedWindow.safeAreaInsets.left > 0 || unwrapedWindow.safeAreaInsets.bottom > 0 {
             // print(unwrapedWindow.safeAreaInsets)
             return true
