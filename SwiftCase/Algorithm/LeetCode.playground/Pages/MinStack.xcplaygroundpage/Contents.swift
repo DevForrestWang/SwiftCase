6fa3304438a77da2bf77ea8d8af9234a239e6770
wangfd <wangfd@gyist.com>
2022-11-17 17:53:54 +0800

    "MF"

--- SwiftCase/Algorithm/LeetCode.playground/Pages/MinStack.xcplaygroundpage/Contents.swift
+++ SwiftCase/Algorithm/LeetCode.playground/Pages/MinStack.xcplaygroundpage/Contents.swift
@@ -3,22 +3,66 @@ import UIKit
 //===----------------------------------------------------------------------===//
 // 题目：剑指 Offer 30. 包含min函数的栈
 // 描述：定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+// 示例：
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.min();   --> 返回 -3
+// minStack.pop();
+// minStack.top();   --> 返回 0
+// minStack.min();   --> 返回 -2
+//
+// peek、top 不改变栈的值(不删除栈顶的值)；pop会把栈顶的值删除
 //===----------------------------------------------------------------------===/
 
 class MinStack {
     init() {}
 
-    func push(_: Int) {}
+    /// 添加元素
+    func push(_ value: Int) {
+        if let minValue = minStackAry.first {
+            if value < minValue {
+                minStackAry.insert(value, at: 0)
+            } else {
+                minStackAry.insert(minValue, at: 0)
+            }
+        } else {
+            minStackAry.insert(value, at: 0)
+        }
+        
+        stackAry.insert(value, at: 0)
+    }
 
-    func pop() {}
+    /// 弹出栈顶元素
+    func pop() {
+        if stackAry.count <= 0 || minStackAry.count <= 0 {
+            return
+        }
+        
+        stackAry.remove(at: 0)
+        minStackAry.remove(at: 0)
+    }
 
-    func top() -> Int {}
+    /// 获取栈顶元素
+    func top() -> Int {
+        return stackAry.first ?? Int.min
+    }
 
-    func min() -> Int {}
+    /// 返回最小元素
+    func min() -> Int {
+        minStackAry.first ?? Int.min
+    }
+    
+    var stackAry: [Int] = []
+    var minStackAry: [Int] = []
 }
 
 let obj = MinStack()
-obj.push(x)
+obj.push(-2)
+obj.push(0)
+obj.push(-3)
+print(obj.min())
 obj.pop()
-let ret_3: Int = obj.top()
-let ret_4: Int = obj.min()
+print(obj.top())
+print(obj.min())
