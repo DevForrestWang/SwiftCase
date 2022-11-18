865dc206bf9bee7ebae1e02abcb30f0757a1618e
wangfd <wangfd@gyist.com>
2022-11-18 10:31:58 +0800

    "MF"

---
+++ SwiftCase/Algorithm/LeetCode.playground/Pages/SolutionList.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//===----------------------------------------------------------------------===//
+// 题目：剑指 Offer 06. 从尾到头打印链表
+// 描述：输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+// 示例：
+// 输入：head = [1,3,2]
+// 输出：[2,3,1]
+//
+//===----------------------------------------------------------------------===/
+
+class Solution {
+    
+    /// Definition for singly-linked list.
+    public class ListNode {
+        public var val: Int
+        public var next: ListNode?
+        public init(_ val: Int) {
+            self.val = val
+            self.next = nil
+        }
+    }
+    
+    public func append(_ node: Node) {
+        
+    }
+    
+    public func reversePrint(_ head: ListNode?) -> [Int] {
+
+    }
+    
+    
+    public typealias Node = ListNode
+    
+}
+
